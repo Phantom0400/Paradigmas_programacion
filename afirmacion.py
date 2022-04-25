65d184d49527c22ca6b6ef9d0fbc8d1f13322601
Alumno <phantom0400@protonmail.com>
2022-04-25 16:50:39 -0500

    Afirmaciones dentro de una función

--- afirmacion.py
+++ afirmacion.py
@@ -3,10 +3,11 @@
 captacion de errores en una función de python con condiciones
 especificas, donde hay un mensaje de error en caso de no
 cumplir dada condición"""
-def aceleracion(d:float = 0, t:float = 0) -> float:
-    assert t < 0, 'No existen tiempos negativos'
+def aceleracion(d:float = 0, t:float = 1) -> float:
+    assert t>= 0, "Siempre los tiempos son mayor o igual a cero"                        
     return d/(t**2)
 a = aceleracion(d = 10, t = 2)
-print(a)
+print('aceleracion(d = 10 [m], 2 [s]) = '+ str(a) + '[m/s²]')
 a_2 = aceleracion(d = 5, t =-1)
+print(a_2)
 #********************************************************
